6cf594b2fae25e8b451a4c518a5e6c1cd7d5854b
Georgii Perepechko 2 <georgii.perepechko@toptal.com>
2022-09-06 01:39:37 +0300

    formatted

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -5,17 +5,18 @@ import {
     ALL_CHAT_INTERACTION_COMMANDS,
 } from '../commands'
 
-export const INTERACTION_CREATE_HANDLER: ClientEventHandler<'interactionCreate'> = {
-    eventName: 'interactionCreate',
-    async execute(interaction: Interaction) {
-        if (interaction.isAutocomplete()) {
-            return ALL_AUTOCOMPLETE_COMMANDS.find(
-                (c) => c.name === interaction.commandName,
-            )?.execute(interaction)
-        } else if (interaction.isChatInputCommand()) {
-            return ALL_CHAT_INTERACTION_COMMANDS.find(
-                (c) => c.name === interaction.commandName,
-            )?.execute(interaction)
-        }
-    },
-}
+export const INTERACTION_CREATE_HANDLER: ClientEventHandler<'interactionCreate'> =
+    {
+        eventName: 'interactionCreate',
+        async execute(interaction: Interaction) {
+            if (interaction.isAutocomplete()) {
+                return ALL_AUTOCOMPLETE_COMMANDS.find(
+                    (c) => c.name === interaction.commandName,
+                )?.execute(interaction)
+            } else if (interaction.isChatInputCommand()) {
+                return ALL_CHAT_INTERACTION_COMMANDS.find(
+                    (c) => c.name === interaction.commandName,
+                )?.execute(interaction)
+            }
+        },
+    }
